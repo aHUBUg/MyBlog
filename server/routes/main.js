6df7ee9efa59f13cbf23cfe3c60ad37809d375d2
Asio-NOah <asionoah@gmail.com>
2023-12-01 14:40:10 +0300

    01-dEC

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,24 +2,59 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-/* /**
+/**
  * GET /
  * HOME
 */
- router.get('', async (req, res) => {
-   const locals = {
-     title: "NodeJs Blog",
-     description: "Simple Blog created with NodeJs, Express & MongoDb."
-   }
+router.get('', async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
 
-   try {
-     const data = await Post.find();
-     res.render('index', { locals, data });
-   } catch (error) {
-     console.log(error);
-   }
+    let perPage = 10;
+    let page = req.query.page || 1;
 
- });
+    const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec({ maxTimeMS: 30000 });
+
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+// router.get('', async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb."
+//   }
+
+//   try {
+//     const data = await Post.find();
+//     res.render('index', { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
 
 
 /**
